14b6a09a477b13a30fb3f7030f1e3e21a74d26f4
Matthew Feickert <matthew.feickert@cern.ch>
2020-08-22 23:59:38 -0500

    Add plot module

--- src/heputils/__init__.py
+++ src/heputils/__init__.py
@@ -1,3 +1,4 @@
 from .version import __version__
+from . import plot
 
 __all__ = ["__version__"]
---
+++ src/heputils/plot.py
@@ -0,0 +1,35 @@
+"""Visualization module."""
+
+import matplotlib.pyplot as plt
+import mplhep.histplot
+
+
+def stack_hist(hists, **kwargs):
+    fig, ax = plt.subplots()
+    bins = hists[0][1]
+    scale_factors = kwargs.pop("scale_factors", None)
+    labels = kwargs.pop("labels", None)
+    color = kwargs.pop("color", None)
+    xlabel = kwargs.pop("xlabel", None)
+    ylabel = kwargs.pop("ylabel", None)
+    title = kwargs.pop("title", None)
+
+    if all(v is not None for v in [labels, scale_factors]):
+        labels = [
+            label if sf == 1 else f"{label} X {sf}"
+            for label, sf in zip(labels, scale_factors)
+        ]
+    if scale_factors is not None:
+        hists = [h[0] * sf for h, sf in zip(hists, scale_factors)]
+    else:
+        hists = [h[0] for h in hists]
+
+    ax = mplhep.histplot(
+        hists, bins=bins, stack=True, histtype="fill", label=labels, color=color
+    )
+    ax.semilogy()
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.legend(loc="best")
+    return fig, ax
