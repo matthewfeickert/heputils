0f73c19c1aa0f0e39659ade07ffc8239ed254010
Matthew Feickert <matthew.feickert@cern.ch>
2020-08-22 19:45:06 -0500

    feat: Add stub of library (#1)
    * Add library with src/ directory layout
    * Add tests with pytest
    * Add CI with GitHub Actions

---
+++ tests/test_cli.py
@@ -0,0 +1,16 @@
+import shlex
+import heputils
+import time
+
+
+def test_version(script_runner):
+    command = "heputils --version"
+    start = time.time()
+    ret = script_runner.run(*shlex.split(command))
+    end = time.time()
+    elapsed = end - start
+    assert ret.success
+    assert heputils.__version__ in ret.stdout
+    assert ret.stderr == ""
+    # make sure it took less than a second
+    assert elapsed < 1.0
