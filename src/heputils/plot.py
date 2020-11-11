b5213746162912018f307f5578ce3488f45c883e
Matthew Feickert <matthew.feickert@cern.ch>
2020-11-10 22:30:17 -0600

    feat: Add uncertainty bands to stack hists (#11)
    * Use hist.Hist histograms instead of numpy histograms for plot.stack_hist
    * Add plotting of the statistical uncertainty on stack hists using ax.bar
    * Add 'logy' kwarg to control if plot.stack_hist vertical axis is log based
    * Update example notebook

--- src/heputils/plot.py
+++ src/heputils/plot.py
@@ -3,7 +3,8 @@
 import matplotlib.pyplot as plt
 from mplhep import histplot
 import mplhep
-import hist
+import numpy as np
+from .convert import stack_hists
 
 
 def set_style(style):
@@ -23,6 +24,37 @@ def set_style(style):
     mplhep.set_style(style)
 
 
+def _plot_uncertainty(model_hist, ax):
+    """
+    Plot the model uncertainity as a bar plot
+
+    Args:
+        model_hist (`hist.Hist`): The histogram to calculate uncertainity for
+        ax (`matplotlib.axes.Axes`): The axis the bar plot is drawn on
+
+    Returns:
+        `matplotlib.axes.Axes`: The axis the bar plot is drawn on
+    """
+
+    stat_uncert = np.sqrt(model_hist)
+    bin_centers = model_hist.axes.centers[0]
+    bin_widths = model_hist.axes.widths[0]
+    uncert_label = "Stat Uncertainity"
+
+    ax.bar(
+        bin_centers,
+        height=2 * stat_uncert,
+        width=bin_widths,
+        bottom=model_hist - stat_uncert,
+        fill=False,
+        linewidth=0,
+        edgecolor="gray",
+        hatch=3 * "/",
+        label=uncert_label,
+    )
+    return ax
+
+
 def stack_hist(hists, **kwargs):
     """
     Plot a stacked histogram of all the input histograms
@@ -36,9 +68,6 @@ def stack_hist(hists, **kwargs):
     """
     if not isinstance(hists, list):
         hists = [hists]
-    if isinstance(hists[0], hist.Hist):
-        hists = [h.to_numpy() for h in hists]
-    bins = hists[0][1]
 
     # get all the kwargs
     scale_factors = kwargs.pop("scale_factors", None)
@@ -51,6 +80,7 @@ def stack_hist(hists, **kwargs):
     xlabel = kwargs.pop("xlabel", None)
     ylabel = kwargs.pop("ylabel", None)
     title = kwargs.pop("title", None)
+    semilogy = kwargs.pop("logy", True)
     return_artists = kwargs.pop("return_artists", False)
 
     fig, ax = plt.subplots()
@@ -61,20 +91,26 @@ def stack_hist(hists, **kwargs):
             for label, sf in zip(labels, scale_factors)
         ]
     if scale_factors is not None:
-        hists = [h[0] * sf for h, sf in zip(hists, scale_factors)]
-    else:
-        hists = [h[0] for h in hists]
+        hists = [h * sf for h, sf in zip(hists, scale_factors)]
 
     histplot(
         hists,
-        bins=bins,
         stack=True,
         histtype="fill",
         label=labels,
         color=color,
         alpha=alpha,
     )
-    ax.semilogy()
+
+    # Inspired by cabinetry
+    # https://github.com/alexander-held/cabinetry/blob/aa36561eba458d47a17a4a7db1ffdce08417ce89/src/cabinetry/contrib/matplotlib_visualize.py#L87
+    stack_hist = stack_hists(hists)
+    _plot_uncertainty(stack_hist, ax)
+
+    if semilogy:
+        ax.semilogy()
+        # Ensure enough space for legend
+        ax.set_ylim(top=max(stack_hist) * 10)
 
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
