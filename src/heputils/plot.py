0d3708592ceca5ef83627b8587fbfaf1922cb777
Matthew Feickert <matthew.feickert@cern.ch>
2020-11-11 14:06:05 -0600

    Add data_hist function

--- src/heputils/plot.py
+++ src/heputils/plot.py
@@ -55,6 +55,51 @@ def _plot_uncertainty(model_hist, ax):
     return ax
 
 
+def data_hist(hist, uncert=None, ax=None, **kwargs):
+    """
+    Plot a histogram styled as data.
+
+    Args:
+        hist (`hist.Hist`): The histogram containing the data
+        uncert (`array`): The uncertainity values for the `hist`
+        ax (`matplotlib.axes.Axes`): The axis object to plot on
+
+    Returns:
+        `matplotlib.axes.Axes`: matplotlib subplot axis object
+    """
+    if uncert is None:
+        uncert = np.sqrt(hist)
+    if ax is None:
+        ax = plt.gca()
+
+    # get all the kwargs
+    color = kwargs.pop("color", "black")
+    label = kwargs.pop("label", "Data")
+    xlabel = kwargs.pop("xlabel", None)
+    ylabel = kwargs.pop("ylabel", None)
+    title = kwargs.pop("title", None)
+    semilogy = kwargs.pop("logy", True)
+    return_artists = kwargs.pop("return_artists", False)
+
+    histplot(
+        hist,
+        yerr=uncert,
+        histtype="errorbar",
+        color=color,
+        label=label,
+        ax=ax,
+    )
+    if semilogy:
+        ax.semilogy()
+
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.legend(loc="best")
+
+    return (ax, ax.get_children()) if return_artists else ax
+
+
 def stack_hist(hists, **kwargs):
     """
     Plot a stacked histogram of all the input histograms
@@ -64,7 +109,7 @@ def stack_hist(hists, **kwargs):
         kwargs: Keyword arguments to matplotlib
 
     Returns:
-        fig, ax: matplotlib subplot figure and axis objects
+        `matplotlib.axes.Axes`: matplotlib subplot axis object
     """
     if not isinstance(hists, list):
         hists = [hists]
@@ -81,9 +126,14 @@ def stack_hist(hists, **kwargs):
     ylabel = kwargs.pop("ylabel", None)
     title = kwargs.pop("title", None)
     semilogy = kwargs.pop("logy", True)
+    ax = kwargs.pop("ax", None)
+    _data_hist = kwargs.pop("data_hist", None)
+    data_uncert = kwargs.pop("data_uncert", None)
+    data_label = kwargs.pop("data_label", "Data")
     return_artists = kwargs.pop("return_artists", False)
 
-    fig, ax = plt.subplots()
+    if ax is None:
+        _, ax = plt.subplots()
 
     if all(v is not None for v in [labels, scale_factors]):
         labels = [
@@ -107,10 +157,13 @@ def stack_hist(hists, **kwargs):
     stack_hist = stack_hists(hists)
     _plot_uncertainty(stack_hist, ax)
 
+    if _data_hist is not None:
+        ax = data_hist(_data_hist, uncert=data_uncert, label=data_label, ax=ax)
+
     if semilogy:
         ax.semilogy()
         # Ensure enough space for legend
-        ax.set_ylim(top=max(stack_hist) * 10)
+        ax.set_ylim(top=max(stack_hist) * 100)
 
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
