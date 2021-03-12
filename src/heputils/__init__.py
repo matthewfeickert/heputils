e78eb508e725345b326c86ddaec4917fc41befc7
Matthew Feickert <matthew.feickert@cern.ch>
2021-03-12 02:12:07 -0600

    Apply isort

--- src/heputils/__init__.py
+++ src/heputils/__init__.py
@@ -1,7 +1,7 @@
-from .version import __version__
-from . import plot
 from . import convert
+from . import plot
 from . import utils
+from .version import __version__
 
 # Satisfy pyflakes
 __all__ = ["__version__", "plot", "convert", "utils"]
