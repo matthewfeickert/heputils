2153b9477a2ba54154fd203925eb272825f9c305
Matthew Feickert <matthew.feickert@cern.ch>
2021-08-05 12:03:34 -0500

    Use fixture

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,15 +1,14 @@
 import hist
 import matplotlib.pyplot as plt
 import numpy as np
+import pytest
 from hist import Hist
 
 import heputils
 
 
-def test_xlabel_passed_through_ax():
-    np.random.seed(0)
-    heputils.plot.set_style("ATLAS")
-
+@pytest.fixture
+def hist_tuple():
     hist_1 = Hist(
         hist.axis.Regular(
             50, -5, 5, name="x", label="x [units]", underflow=False, overflow=False
@@ -29,7 +28,15 @@ def test_xlabel_passed_through_ax():
         storage=hist.storage.Weight(),
     ).fill(np.random.normal(size=1000), weight=1.0)
 
-    hists = [hist_1, hist_2]
+    return hist_1, hist_2, hist_3
+
+
+def test_data_hist_xlabel_passed_through_ax(hist_tuple):
+    np.random.seed(0)
+    heputils.plot.set_style("ATLAS")
+
+    hists = list(hist_tuple[:2])
+    hist_3 = hist_tuple[-1]
 
     fig, ax = plt.subplots()
     ax = heputils.plot.stack_hist(
